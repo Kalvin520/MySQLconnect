83e0f7c3a6cc260167619ee8ac3d9344f23e1f1b
Kalvin <yaojaikai520@gmail.com>
2022-10-25 17:31:38 +0800

    renew

--- MySQLTest/src/main/java/DBSource.java
+++ MySQLTest/src/main/java/DBSource.java
@@ -1,2 +1,7 @@
-public class DBSource {
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface DBSource {
+        public Connection getconnection() throws SQLException;
+        public  void closeConnection(Connection connection)throws SQLException;
 }
