fb91ec2a60609e405c054f09c959eed74de0a12d
Kalvin <yaojaikai520@gmail.com>
2022-10-25 11:48:10 +0800

    add DBSource

--- MySQLTest/src/main/java/main.java
+++ MySQLTest/src/main/java/main.java
@@ -3,16 +3,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class main {
-    public static String url = "jdbc.mysql://localhost:3306/mysql_DB";
-    public static String user = "root";
-    public static String password = "tv228222";
 
-    public static void main(String[] args) throws SQLException {
-        try {
-            Connection connection = DriverManager.getConnection(url, user, password);
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
+    public static void main(String[] args) {
+        Connection connection = source.getConnection();
+        source.close(connection);
+        System.out.println("Done.");
     }
-
 }
---
+++ MySQLTest/src/main/java/source.java
@@ -0,0 +1,43 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class source {
+    public static String url = "jdbc:mysql://localhost:3306/mysql_DB?";
+    public static String user = "root";
+    public static String password = "tv228222";
+
+
+    //在屬性區宣告靜態區塊(類別建立只能引入一次)
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("程式驅動找不到");
+            e.printStackTrace();
+
+        }
+    }
+    //建立取得資料庫連結物件靜態方法
+    public static Connection getConnection() {
+        Connection connection = null;
+        //取得資料庫連接物件
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+            System.out.println("connection open.");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return connection;
+    }
+    //      關閉資料
+    public static void close(Connection connection){
+        try {
+            connection.close();
+            System.out.println("connection close.");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+}
